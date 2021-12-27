766dd3c4e1624d3bbca450a780a3594c8ae9d08a
marlene32100 <67315304+marlene32100@users.noreply.github.com>
2021-12-27 14:36:42 +0100

    Added react-responsive and fixed topbar on small screens

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./topbar.scss";
+import MediaQuery from "react-responsive";
 import logo from "../../media/qualis_logo.jpg";
 import DehazeIcon from "@mui/icons-material/Dehaze";
 
@@ -7,20 +8,48 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
     <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
-        <div className="left">
-          <a href="#contact">
-            <div className="logo">
-              <img src={logo} alt="Qualis Italica logo" className="logo-img" />
-            </div>
-          </a>
-        </div>
-        <div className="center">
-          <h1>Qualis Italica</h1>
-          <h2 className="description">Restaurant Project Management</h2>
-        </div>
-        <div className="right">
-          <DehazeIcon className="menu" onClick={() => setMenuOpen(!menuOpen)} />
-        </div>
+        <MediaQuery minWidth={1224}>
+          <div className="left">
+            <a href="#contact">
+              <div className="logo">
+                <img
+                  src={logo}
+                  alt="Qualis Italica logo"
+                  className="logo-img"
+                />
+              </div>
+            </a>
+          </div>
+          <div className="center">
+            <h1>Qualis Italica</h1>
+            <h2 className="description">Restaurant Project Management</h2>
+          </div>
+          <div className="right">
+            <DehazeIcon
+              className="menu"
+              onClick={() => setMenuOpen(!menuOpen)}
+            />
+          </div>
+        </MediaQuery>
+        <MediaQuery maxWidth={1224}>
+          <div className="left">
+            <a href="#contact">
+              <div className="logo">
+                <img
+                  src={logo}
+                  alt="Qualis Italica logo"
+                  className="logo-img"
+                />
+              </div>
+            </a>
+          </div>
+          <div className="right">
+            <DehazeIcon
+              className="menu"
+              onClick={() => setMenuOpen(!menuOpen)}
+            />
+          </div>
+        </MediaQuery>
       </div>
     </div>
   );
